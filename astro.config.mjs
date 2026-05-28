2048b0e81c775142997fe9e697a41189f3848753
Beto <juanalbertomor@gmail.com>
2026-05-28 15:29:58 +0200

    fix: add Vercel adapter for static routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   site: 'https://betomoreno.com',
   integrations: [mdx(), sitemap()],
+  output: 'static',
+  adapter: vercel(),
 });
